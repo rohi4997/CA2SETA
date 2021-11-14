0e77edb3d40c46c737ad86e96f184fcdef02ba66
rohit <rohitrkd4997@gmail.com>
2021-11-14 14:58:11 +0530

    initial commit

---
+++ Phy.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import math
+from array import *
+
+x = array('f', [])
+y = array('f', [])
+h = 6.62e-34
+k = 1.38e-23
+c = 3e8
+T = 3000
+ld = 2e-7
+
+for i in range(200):
+    r = ((8*math.pi*h*c)/math.pow(ld, 5))*(1/(math.exp((h*c)/(ld*k*T)) -1))
+    x.append(ld)
+    y.append(r)
+    ld += 0.2e-7
+
+
+plt.plot(x, y)
+plt.xlabel('Wavelength')
+plt.ylabel('Spectral energy density')
+plt.title('Blackbody Radiation')
+plt.show()
